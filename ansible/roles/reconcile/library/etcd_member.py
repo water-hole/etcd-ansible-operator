6d61190fc20b2ee68df16a9c968a5eaa4a15b498
Alaykumar Patel <alay1431@gmail.com>
2018-11-13 15:19:50 -0500

    Add backup, restore features and remove lookup role

--- ansible/roles/reconcile/library/etcd_member.py
+++ ansible/roles/reconcile/library/etcd_member.py
@@ -53,8 +53,8 @@ EXAMPLES = '''
 # Pass in a message
 - name: Add a member to etcd cluster reachable at 192.168.39.66:32379
   etcd:
-    name: hello world
     state: "present",
+    type:  "member"
     cluster_host: "192.168.39.66",
     cluster_port: "32379",
     name: "hello-world",
@@ -67,9 +67,20 @@ EXAMPLES = '''
 - name: Remove a member from etcd cluster reachable at 192.168.39.66:32379
   etcd:
     state: "absent",
+    type:  "member"
     cluster_host: "192.168.39.66",
     cluster_port: "32379",
     id: "14181629488891917781"
+
+
+- name: Create backup of cluster reachable at 192.168.39.66:32379
+  etcd:
+    state: "present",
+    type:  "backup"
+    cluster_host: "192.168.39.66",
+    cluster_port: "32379",
+    filename: "/var/etcd/hello-cluster.backup"
+
 
 '''
 
@@ -90,7 +101,9 @@ def run_module():
     # the module
     module_args = dict(
         state=dict(type='str', required=True),
+        type=dict(type='str', required=True),
         name=dict(type='str', required=False, default=''),
+        filename=dict(type='str', required=False, default=''),
         id=dict(type='str', required=False, default=''),
         ca_cert=dict(type='str', required=False, default=None),
         cert_key=dict(type='str', required=False, default=None),
@@ -119,7 +132,7 @@ def run_module():
     if module.check_mode:
         return result
 
-    if module.params['state'] == 'present':
+    if module.params["type"] == "member" and module.params['state'] == 'present':
         try:
             members = etcd_member_present(cluster_host=module.params['cluster_host'],
                                           cluster_port=module.params['cluster_port'],
@@ -131,13 +144,14 @@ def run_module():
             result['members'] = [
                 dict(id=m.id, name=m.name, peer_urls=list(m.peer_urls), client_urls=list(m.client_urls))
                 for m in members]
+            module.exit_json(**result)
         except Exception as err:
             if str(err) == 'name':
                 module.fail_json(msg='name is empty', **result)
             if str(err) == 'peer_urls':
                 module.fail_json(msg='peer_urls is empty', **result)
 
-    elif module.params['state'] == 'absent':
+    elif module.params["type"] == "member" and module.params['state'] == 'absent':
         try:
             members = etcd_member_absent(cluster_host=module.params['cluster_host'],
                                          cluster_port=module.params['cluster_port'],
@@ -149,11 +163,29 @@ def run_module():
             result['members'] = [
                 dict(id=m.id, name=m.name, peer_urls=list(m.peer_urls), client_urls=list(m.client_urls))
                 for m in members]
+            module.exit_json(**result)
         except ValueError as err:
             module.fail_json(msg=str(err), **result)
         except Exception as err:
             if str(err) == 'id':
                 module.fail_json(msg='id is not set', **result)
+    elif module.params["type"] == "backup" and module.params["state"] == "present":
+        try:
+            backup = etcd_backup_present(cluster_host=module.params['cluster_host'],
+                                         cluster_port=module.params['cluster_port'],
+                                         filename=module.params['filename'],
+                                         ca_cert=module.params['ca_cert'],
+                                         cert_cert=module.params['cert_cert'],
+                                         cert_key=module.params['cert_key'])
+            module.exit_json(changed=True)
+        except Exception as err:
+            if str(err) == 'filename':
+                module.fail_json(msg='name is empty', **result)
+            if str(err) == 'peer_urls':
+                module.fail_json(msg='peer_urls is empty', **result)
+            if str(err) == "connection":
+                module.fail_json(msg='invalid connection host or port', **result)
+
     module.exit_json(**result)
 
 
@@ -163,23 +195,52 @@ def etcd_member_present(**kwargs):
     if len(kwargs['peer_urls']) == 0:
         raise Exception('peer_urls')
 
-    client = connect_etcd_cluster(kwargs['cluster_host'], kwargs['cluster_port'], kwargs['ca_cert'], kwargs['cert_key'],
-                                  kwargs['cert_cert'])
-    add_member(client, kwargs['peer_urls'])
+    try:
+        client = connect_etcd_cluster(kwargs['cluster_host'], kwargs['cluster_port'], kwargs['ca_cert'],
+                                      kwargs['cert_key'],
+                                      kwargs['cert_cert'])
+        add_member(client, kwargs['peer_urls'])
+
+    except etcd3.exceptions.ConnectionFailedError:
+        raise Exception("connection")
+
     return client.members
 
 
 def etcd_member_absent(**kwargs):
     if kwargs['id'] == -1:
         raise Exception('id')
-    client = connect_etcd_cluster(kwargs['cluster_host'], kwargs['cluster_port'], kwargs['ca_cert'], kwargs['cert_key'],
-                                  kwargs['cert_cert'])
-    if kwargs['id'][0:2] == '0x':
-        _id = int(kwargs['id'], 16)
-    else:
-        _id = int(kwargs['id'])
-    remove_member(client, _id)
-    return client.members
+    try:
+        client = connect_etcd_cluster(kwargs['cluster_host'], kwargs['cluster_port'], kwargs['ca_cert'],
+                                      kwargs['cert_key'],
+                                      kwargs['cert_cert'])
+        if kwargs['id'][0:2] == '0x':
+            _id = int(kwargs['id'], 16)
+        else:
+            _id = int(kwargs['id'])
+        remove_member(client, _id)
+        return client.members
+    except etcd3.exceptions.ConnectionFailedError:
+        raise Exception("connection")
+
+
+def etcd_backup_present(**kwargs):
+    if kwargs['filename'] == '':
+        raise Exception('filename')
+    try:
+        client = connect_etcd_cluster(kwargs['cluster_host'], kwargs['cluster_port'], kwargs['ca_cert'],
+                                      kwargs['cert_key'],
+                                      kwargs['cert_cert'])
+        backup = create_backup(client, kwargs['filename'])
+        return backup
+    except etcd3.exceptions.ConnectionFailedError:
+        raise Exception("connection")
+
+
+def create_backup(client, filename):
+    with open(filename, 'wb+') as f:
+        snapshot = client.snapshot(f)
+    return snapshot
 
 
 def connect_etcd_cluster(host, port, ca_cert=None, cert_key=None, cert_cert=None):
